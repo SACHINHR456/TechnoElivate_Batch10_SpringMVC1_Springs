0639fbe1fcf37fe4dda960b32ff3343da664cb7b
SACHINHR456 <sachinhr456@gmail.com>
2021-05-01 14:41:01 +0530

    adding the spring mvc prog

---
+++ src/main/java/com/te/springmvc1/App.java
@@ -0,0 +1,5 @@
+package com.te.springmvc1;
+
+public class App {
+
+}
---
+++ src/main/java/com/te/springmvc1/beans/UserBean.java
@@ -0,0 +1,11 @@
+package com.te.springmvc1.beans;
+
+import lombok.Data;
+
+@Data
+public class UserBean {
+	
+	private String user;
+	private String pwd;
+
+}
